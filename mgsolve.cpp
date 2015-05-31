8c53a8be6ed73bfc4d7e7ac44f1973f2b0352f3b
karnajitsen <karnajitsen@gmail.com>
2015-06-01 00:00:00 +0200

    upd

--- mgsolve.cpp
+++ mgsolve.cpp
@@ -295,7 +295,8 @@ void mgsolve(size_t level)
 	size_t i = 0;
     init(hsize, level);
 	sGrid = new Grid(gdim, gdim, hsize, hsize, true);
-#pragma omp parallel firstprivate(gdim,hsize)
+	size_t k;
+#pragma omp parallel private(k) firstprivate(gdim,hsize)
 	{
 #pragma omp for
 		for (size_t k = 0; k < gdim; k++)
